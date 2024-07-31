157b856caf5ab233f01a4fa94075f525cfe58757
Ivan Warlock <Ivan.Warlock@ibm.com>
2024-07-30 19:14:31 -0600

    Comments on factory aes

--- creationalClasses/factory_aes.py
+++ creationalClasses/factory_aes.py
@@ -8,19 +8,39 @@
 from Cryptodome.Cipher import AES
 from Crypto.Util import Counter
 from Crypto.Random import get_random_bytes
-from abc import abstractmethod
+from abc import abstractmethod, property
 
 import random
 import string
 
 
 class aes():
-    
+    """
+        aes class for data encryption/decryption used as factory creation class pattern.       
+            
+        *Attributes:
+            key [string] \n
+            iv  [string] \n
+            mode [string] \n
+            counter [string] \n
+            modes_aes [dict]
+
+        *Methods:
+            encrypt(self, data2Encrypt=""); \n
+            setMode(self, mode_aes = ""); \n
+            getMode(self); \n
+            setKey(self); \n
+            getKey(self); \n
+            setIV(self); \n
+            getIV(self); \n
+            setCounter(self); \n
+            getCounter(self)  \n
+            pad(self,data)
+    """
     key = "";
     iv = "";
     mode = "";
     counter = "";
-    
     modes_aes = {
         "CBC": AES.MODE_CBC,
         "ECB": AES.MODE_ECB,
@@ -31,16 +51,24 @@ class aes():
     
     @abstractmethod
     def encrypt(self, data2Encrypt = ""):
-        print("Raw Data: ",data2Encrypt);
-        print("Key: ",self.key);
-        print("IV: ",self.iv);
-        cipher = AES.new(bytes(self.key,"utf-8"), self.mode, iv=bytes(self.iv,'UTF-8'))
-        cipher_text = cipher.encrypt(bytes(self.pad(data2Encrypt),'UTF-8'))
-        print("Data encrypted: ",cipher_text.hex())
-    
+        try:
+            print("Raw Data: ",data2Encrypt);
+            print("Key: ",self.key);
+            print("IV: ",self.iv);
+            cipher = AES.new(bytes(self.key,"utf-8"), self.mode, iv=bytes(self.iv,'UTF-8'))
+            cipher_text = cipher.encrypt(bytes(self.pad(data2Encrypt),'UTF-8'))
+            print("Data encrypted: ",cipher_text.hex());
+        except Exception as e: 
+            print("Error at [creationalClasses][factory_aes][encrypt].-");
+            print(e);
+            
     @abstractmethod
     def setMode(self, mode_aes = ""):
-        self.mode = self.modes_aes[mode_aes];
+        try:
+            self.mode = self.modes_aes[mode_aes];
+        except Exception as e: 
+            print("Error at [creationalClasses][factory_aes][setMode].-");
+            print(e);
     
     @abstractmethod
     def getMode(self) -> str:
@@ -48,7 +76,12 @@ class aes():
     
     @abstractmethod
     def setKey(self):
-        self.key = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(32))
+        try:
+            self.key = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(32));
+        except Exception as e: 
+            print("Error at [creationalClasses][factory_aes][setKey].-");
+            print(e);
+    
     
     @abstractmethod
     def getKey(self) -> str:
@@ -56,7 +89,11 @@ class aes():
     
     @abstractmethod
     def setIV(self):
-        self.iv = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(16))
+        try:
+            self.iv = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(16))
+        except Exception as e: 
+            print("Error at [creationalClasses][factory_aes][setIV].-");
+            print(e);
     
     @abstractmethod
     def getIV(self) -> str:
@@ -64,8 +101,12 @@ class aes():
     
     @abstractmethod
     def setCounter(self):
-        nonce = get_random_bytes(8)
-        self.counter = Counter.new(64, prefix=nonce)
+        try:
+            nonce = get_random_bytes(8)
+            self.counter = Counter.new(64, prefix=nonce)
+        except Exception as e: 
+            print("Error at [creationalClasses][factory_aes][setCounter].-");
+            print(e);
     
     @abstractmethod
     def getCounter(self) -> bytes:
@@ -73,9 +114,14 @@ class aes():
     
     @abstractmethod
     def pad (self,data):
-        data = ' '*16 + data
-        pad = 16 - len(data) % 16
-        return data + pad * chr(pad)
+        try:
+            data = ' '*16 + data
+            pad = 16 - len(data) % 16
+            return data + pad * chr(pad)
+        except Exception as e: 
+            print("Error at [creationalClasses][factory_aes][pad].-");
+            print(e);
+            return "";
     
 class aes_CBC(aes):
     def printmode(self):
@@ -85,7 +131,7 @@ class aes_ECB(aes):
     #Electronic CodeBook
     def printmode(self):
         print("Running Encryption AES 256 ECB");
-    
+        
     def encrypt(self, data2Encrypt = ""):
         print("Raw Data: ",data2Encrypt);
         print("Key: ",self.key);
